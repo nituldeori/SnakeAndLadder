f10d1c41b2b5905ced2f9329548a4d3ebeed8b02
unknown <nituldeori63@gmail.com>
2020-11-22 00:20:05 +0530

    Calculated the no of times dice was rolled to win and the position after each die roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,7 @@ public class SnakeLadder {
 		System.out.println("******** Welcome to Snake and Ladder Game *********");
 		int player1_pos=0;
 		System.out.println("Starting position of player1: "+player1_pos);
+		int num_dice_thrown=0;
 		while(player1_pos!=100) {
 		    int diceRoll=ThreadLocalRandom.current().nextInt(1,7);
 		    int noPlay=0;
@@ -26,8 +27,11 @@ public class SnakeLadder {
 			else
 			    player1_pos-=diceRoll;
 		    }
+		    System.out.println("Position after die roll: "+player1_pos);
+		    num_dice_thrown++;
 		
 	    }
+		System.out.println("Number of times dice was rolled to win: "+num_dice_thrown);
 		System.out.println("Congratulations! You reached the 100 position and won");
 	}
 
