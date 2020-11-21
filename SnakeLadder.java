4d6e5a1a436d3e0b6551602cff565bdf64517eaa
unknown <nituldeori63@gmail.com>
2020-11-21 21:53:10 +0530

    Used random function to get number between 1 and 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,12 @@
 package snakeLadder;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("******** Welcome to Snake and Ladder Game *********");
 		int player1_pos=0;
 		System.out.println("Starting position of player1: "+player1_pos);
+		int diceRoll=ThreadLocalRandom.current().nextInt(1,7);
+		System.out.println("Number got after dice roll: "+diceRoll);
 	}
 
 }
