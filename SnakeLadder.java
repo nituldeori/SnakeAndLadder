57a411ba01570892d5786a58a8ca136d481c0569
unknown <nituldeori63@gmail.com>
2020-11-21 23:49:16 +0530

    Checking for no play, ladder or snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,6 +7,22 @@ public class SnakeLadder {
 		System.out.println("Starting position of player1: "+player1_pos);
 		int diceRoll=ThreadLocalRandom.current().nextInt(1,7);
 		System.out.println("Number got after dice roll: "+diceRoll);
+		int noPlay=0;
+		int ladder=1;
+		int luckRoll=ThreadLocalRandom.current().nextInt(-1,2);
+		if(luckRoll==noPlay) {
+			player1_pos+=0;
+			System.out.println("Sorry its no play!");
+		}
+		else if(luckRoll==ladder) {
+			player1_pos+=diceRoll;
+			System.out.println("Bravo! Its a ladder");
+		}
+		else {
+			player1_pos-=diceRoll;
+			System.out.println("Ouch! You landed on a snake");
+		}
+		System.out.println("Player position after dice and luck roll: "+player1_pos);
 	}
 
 }
