d55ac5b338b95904d61ecc6f9278182ce0db8679
unknown <nituldeori63@gmail.com>
2020-11-22 00:06:23 +0530

    Repeat dice rolling until player reaches 100th position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,24 +5,27 @@ public class SnakeLadder {
 		System.out.println("******** Welcome to Snake and Ladder Game *********");
 		int player1_pos=0;
 		System.out.println("Starting position of player1: "+player1_pos);
-		int diceRoll=ThreadLocalRandom.current().nextInt(1,7);
-		System.out.println("Number got after dice roll: "+diceRoll);
-		int noPlay=0;
-		int ladder=1;
-		int luckRoll=ThreadLocalRandom.current().nextInt(-1,2);
-		if(luckRoll==noPlay) {
-			player1_pos+=0;
-			System.out.println("Sorry its no play!");
-		}
-		else if(luckRoll==ladder) {
-			player1_pos+=diceRoll;
-			System.out.println("Bravo! Its a ladder");
-		}
-		else {
-			player1_pos-=diceRoll;
-			System.out.println("Ouch! You landed on a snake");
-		}
-		System.out.println("Player position after dice and luck roll: "+player1_pos);
+		while(player1_pos!=100) {
+		    int diceRoll=ThreadLocalRandom.current().nextInt(1,7);
+		    int noPlay=0;
+		    int ladder=1;
+		    int luckRoll=ThreadLocalRandom.current().nextInt(-1,2);
+		    if(luckRoll==noPlay) {
+			    player1_pos+=0;
+		    }
+		    else if(luckRoll==ladder) {
+			    player1_pos+=diceRoll;
+		    }
+		    else {
+			    if(player1_pos-diceRoll<0) {
+				    player1_pos=0;
+			    }
+			else
+			    player1_pos-=diceRoll;
+		    }
+		
+	    }
+		System.out.println("Congratulations! You reached the 100 position and won");
 	}
 
 }
