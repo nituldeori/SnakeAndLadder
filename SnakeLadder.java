7ffa0f77267186729c63a9dbc1d1d59bb549d0a4
unknown <nituldeori63@gmail.com>
2020-11-21 21:43:47 +0530

    Showed the starting position of player1

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,8 @@ package snakeLadder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("******** Welcome to Snake and Ladder Game *********");
+		int player1_pos=0;
+		System.out.println("Starting position of player1: "+player1_pos);
 	}
 
 }
